0cdeb7b901e18ae529b2fce596d71bd0ddf78b9f
MSI\wases <sytimi.030211@icloud.com>
2025-10-24 15:34:24 +0900

    Text_Botuun

--- Form_test/Form_test/Form1.cs
+++ Form_test/Form_test/Form1.cs
@@ -15,6 +15,28 @@ namespace Form_test
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 6; i++)//四回繰り返す
+            for (int j = 0; j < 4; j++)
+            {    //インスタンスの設定
+                Test_Buttoncs testButton = new Test_Buttoncs();
+                //ボタンの位置を設定
+
+
+                testButton.Location = new Point(50 * i, 50*j);
+
+                //ボタンの大きさを設定
+                testButton.Size = new Size(50, 50);
+                //ボタン内のテキスト            
+                testButton.Text = "ぼたん２";
+
+                //コントロール内にボタンを設定
+                Controls.Add(testButton);
+            }
         }
+                private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("高市早苗");
+        }
+       
     }
 }
---
+++ Form_test/Form_test/_Test Buttoncs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_test
+{
+    internal class Test_Buttoncs: Button
+    {
+
+    }
+}
